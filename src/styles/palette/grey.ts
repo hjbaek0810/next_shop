1e440e369d075051644f277044d8524d680297da
hjbaek0810 <doolgi0810@naver.com>
2024-08-01 19:20:20 +0900

    Style: 색상 추가

---
+++ src/styles/palette/grey.ts
@@ -0,0 +1,15 @@
+const Grey = {
+  '00': '#373737',
+  10: '#575757',
+  20: '#6E6E6E',
+  30: '#7F7F7F',
+  40: '#8D8D8D',
+  50: '#A4A4A4',
+  60: '#BFBFBF',
+  70: '#D1D1D1	',
+  80: '#E0E0E0	',
+  90: '#EEEEEE',
+  95: '#F3F3F3',
+} as const;
+
+export default Grey;
---
+++ src/styles/palette/index.ts
@@ -0,0 +1,5 @@
+import Grey from './grey';
+
+export const ColorPalettes = {
+  Grey,
+} as const;
