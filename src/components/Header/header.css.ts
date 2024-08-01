13f1165dd3bb9a4a32f2d3e0d800637c20007902
hjbaek0810 <doolgi0810@naver.com>
2024-08-01 16:43:19 +0900

    Feat: 헤더바 개발

--- src/components/Header/header.css.ts
+++ src/components/Header/header.css.ts
@@ -1,13 +1,57 @@
 import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
-export const HEADER_BAR_HEIGHT = '5.4rem';
+import { tokens } from '@styles/token.css';
+
+export const HEADER_BAR_HEIGHT = '8rem';
 
 export const header = style({
   position: 'fixed',
   left: 0,
   top: 0,
-  padding: '1.6rem',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: tokens.gap['spacing-004'],
+  padding: tokens.gap['spacing-016'],
   width: '100%',
   height: HEADER_BAR_HEIGHT,
-  backgroundColor: 'pink',
+});
+
+export const logo = style({
+  fontWeight: 'bold',
+  fontSize: tokens.fontSize['font-size-020'],
+});
+
+export const bar = style({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  gap: tokens.gap['spacing-008'],
+});
+
+export const menu = style({
+  display: 'flex',
+  gap: tokens.gap['spacing-012'],
+  fontSize: tokens.fontSize['font-size-014'],
+});
+
+const activeStyle = {
+  color: 'grey',
+  fontWeight: 600,
+};
+
+export const menuItem = recipe({
+  base: {
+    ':active': activeStyle,
+    ':hover': activeStyle,
+  },
+  variants: {
+    selected: {
+      true: activeStyle,
+      false: {},
+    },
+  },
+  defaultVariants: { selected: false },
 });
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,104 @@
+'use client';
+
+import {
+  faCartShopping,
+  faHeart,
+  faSearch,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import * as css from './header.css';
 
+type menuListType = {
+  path: string;
+  label: string;
+};
+
+const MENU_LIST: Array<menuListType> = [
+  {
+    path: '/',
+    label: 'HOME',
+  },
+  {
+    path: '/new',
+    label: 'NEW',
+  },
+  {
+    path: '/best',
+    label: 'BEST',
+  },
+  {
+    path: '/outer',
+    label: 'OUTER',
+  },
+  {
+    path: '/top',
+    label: 'TOP',
+  },
+  {
+    path: '/bottom',
+    label: 'BOTTOM',
+  },
+  {
+    path: '/ops-sk',
+    label: 'OPS/SK',
+  },
+  {
+    path: '/acc',
+    label: 'ACC',
+  },
+];
+
 const Header = () => {
+  const pathname = usePathname();
+  const selected = (menuPath: string) => menuPath === pathname;
+
   return (
     <div className={css.header}>
-      <div>logo</div>
-      {/* <ul>
-				<li>Login</li>
-				<li>Like</li>
-				<li>Cart</li>
-			</ul> */}
+      {/* TODO : logo img */}
+      <Link className={css.logo} href="/">
+        Logo
+      </Link>
+
+      <div className={css.bar}>
+        <ul className={css.menu}>
+          {MENU_LIST.map(({ label, path }) => (
+            <li
+              key={label}
+              className={css.menuItem({ selected: selected(path) })}
+            >
+              <Link href={path}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+
+        <ul className={css.menu}>
+          <li>
+            {/* TODO : search 기능 */}
+            <button>
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </li>
+          <li>
+            <Link href="/my-account">
+              <FontAwesomeIcon icon={faUser} />
+            </Link>
+          </li>
+          <li>
+            <Link href="/cart">
+              <FontAwesomeIcon icon={faCartShopping} />
+            </Link>
+          </li>
+          <li>
+            <Link href="/like">
+              <FontAwesomeIcon icon={faHeart} />
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
