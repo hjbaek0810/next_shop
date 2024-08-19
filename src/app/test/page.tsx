c4f00a84ff9e8aea028762b534d508002ef27015
hjbaek0810 <doolgi0810@naver.com>
2024-08-19 19:55:38 +0900

    Chore: db 연결 테스트

---
+++ src/app/test/page.tsx
@@ -0,0 +1,15 @@
+'use server';
+
+import { getPosts } from '../../../_actions/postActions';
+
+const Test = async () => {
+  const { data, errMsg } = await getPosts();
+
+  if (errMsg) return <h1>{errMsg}</h1>;
+
+  console.log(data);
+
+  return <h1>TEST</h1>;
+};
+
+export default Test;
